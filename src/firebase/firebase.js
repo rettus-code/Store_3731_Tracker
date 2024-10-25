64336a784808864f8b70949518fffb532b359efa
rettusm <rettusm@wwu.edu>
2024-10-25 06:13:00 -0700

    update

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPABSDgD5_HmLhXmnByyG-3OcSR8S66YI",
+  authDomain: "mcd-training.firebaseapp.com",
+  databaseURL: "https://mcd-training-default-rtdb.firebaseio.com",
+  projectId: "mcd-training",
+  storageBucket: "mcd-training.appspot.com",
+  messagingSenderId: "485526086205",
+  appId: "1:485526086205:web:bf2eaf622ab96635dcc054",
+  measurementId: "G-6V9TMMQLZJ"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+
+
+export { app, auth };
